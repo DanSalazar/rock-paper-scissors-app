74ac85dd24d88590572a12785c84d8c73ab784f8
Dan <danielsalazar.cc@gmail.com>
2021-06-21 22:54:41 -0400

    Init

---
+++ src/GlobalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        font-family: 'Barlow Semi Condensed', sans-serif;
+        background: radial-gradient(hsl(214, 47%, 23%), hsl(237, 49%, 15%));
+        font-size: 16px;
+    }
+
+`
