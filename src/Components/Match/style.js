cfeb900d6f6f054f6f468cf5123cc8fa41adb409
Dan <danielsalazar.cc@gmail.com>
2021-11-30 22:48:28 -0400

    feat(src): Add anim in option overlay

--- src/Components/Match/style.js
+++ src/Components/Match/style.js
@@ -1,79 +1,87 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { show } from '../styles'
 
+const overlayAnim = keyframes`
+  0% {
+    background-color: rgba(0, 0, 0, 30%);
+  }
+
+  50% {
+    background-color: rgba(0, 0, 0, 40%);
+  }
+
+  100% {
+    background-color: rgba(0, 0, 0, 30%);
+  }
+`
+
 export const OptionsMatch = styled.div`
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: repeat(2, 1fr);
-    width: 100%;
-    height: auto;
-    animation: ${show} var(--transition-mode);
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  width: 100%;
+  height: auto;
+  animation: ${show} var(--transition-mode);
 
-    @media screen and (min-width: 1024px){
-        grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
-        grid-template-rows: repeat(1, 1fr);
-    }
+  @media screen and (min-width: 1024px){
+    grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
+    grid-template-rows: repeat(1, 1fr);
+  }
 
-    @media screen and (min-width: 1440px){
-        width: 70%;
-    }
+  @media screen and (min-width: 1440px){
+    width: 70%;
+  }
 `
 
 export const OptionMatchWrapper = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-direction: column;
-    transition: 0.2s ease;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+
+  span {
+    color: #fff;
+    margin: 1em 0;
+    text-transform: uppercase;
+    font-size: 0.875em;
+  }
+
+  @media screen and (min-width: 1024px){
+    flex-direction: column-reverse;
 
     span {
-        color: #fff;
-        display: block;
-        margin: 1em 0;
-        text-transform: uppercase;
-        font-size: 0.875em;
-        letter-spacing: 1.5px;
-        text-align: center;
+        font-size: 1.25em;
+        margin: 0 0 2em 0;
     }
-
-    @media screen and (min-width: 1024px){
-        flex-direction: column-reverse;
-
-        span {
-            font-size: 1.25em;
-            margin: 0 0 2em 0;
-        }
-    }
+  }
 `
 
 export const OptionOverlay = styled.div`
-    border-radius: 50%;
-    background: rgba(0, 0, 0, 20%);
-    padding: 0.875em;
+  border-radius: 50%;
+  background-color: rgba(0, 0, 0, 30%);
+  padding: 0.875em;
+  animation: ${overlayAnim} 1.5s var(--transition-mode) infinite;
+
+  div {
+    width: 100px;
+    height: 100px; 
+  }
+
+  @media screen and (max-width: 320px){
+    padding: 0.6em;
 
     div {
-        width: 100px;
-        height: 100px;
+      width: 65px;
+      height: 65px;
     }
+  }
 
-    @media screen and (max-width: 320px){
-        div {
-            width: 65px;
-            height: 65px;
-        }
-    }
+  @media screen and (min-width: 1024px){
+      padding: 2.25em;
 
-    @media screen and (max-width: 320px){
-        padding: 0.6em;
-    }
-
-    @media screen and (min-width: 1024px){
-        padding: 2.25em;
-
-        div {
-            width: ${props => props.sizeD};
-            height: ${props => props.sizeD}; 
-        }
-    }
-
+      div {
+        width: 220px;
+        height: 220px; 
+      }
+  }
 `
